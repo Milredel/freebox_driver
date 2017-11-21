b879968a1acf8ca341ebe614d16bbb334d8e3d76
morgan.fromentin <morgan.fromentin@ezbeez.eu>
2017-11-21 15:09:10 +0100

    simple implementation of some buttons

--- controller.js
+++ controller.js
@@ -1,10 +1,34 @@
 'use strict';
 
+const request = require("request");
+const config = require('./config');
+
 /*
  * Device Controller
  * Events on that device from the Brain will be forwarded here for handling.
  */
 module.exports.onButtonPressed = function onButtonPressed(name, deviceId) {
-  console.log(`[CONTROLLER] ${name} button pressed for device ${deviceId}`);
-  // TODO implement the actions for your device here
+    console.log(`[CONTROLLER] ${name} button pressed for device ${deviceId}`);
+    var actionName = name.replace("button-", "");
+    if (config.freebox.mapping[actionName] != undefined) {
+        sendRequest(config.freebox.mapping[actionName]);
+    }
 };
+
+function sendRequest(keyValue) {
+    try {
+        console.log("preparing and lauching request");
+        request({
+            uri: "http://"+config.freebox.box_identifier+".freebox.fr/pub/remote_control?code="+config.freebox.remote_code+"&key="+keyValue,
+            method: "GET",
+            timeout: 10000,
+            followRedirect: true,
+            maxRedirects: 10
+        }, function(error, response, body) {
+            if (error) console.log(error);
+            console.log("request sent");
+        });
+    } catch (e) {
+        console.log(e);
+    }
+}
--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const neeoapi = require('neeo-sdk');
 const controller = require('./controller');
 
-console.log('NEEO SDK Example "simpleAccessory" adapter');
+console.log('Freebox adapter');
 console.log('------------------------------------------');
 
 /*
@@ -12,15 +12,33 @@ console.log('------------------------------------------');
  */
 
 // first we set the device info, used to identify it on the Brain
-const customLightDevice = neeoapi.buildDevice('Simple Accessory')
-  .setManufacturer('NEEO')
-  .addAdditionalSearchToken('foo')
+const freeboxDevice = neeoapi.buildDevice('Freebox Revolution')
+  .setManufacturer('FREE')
+  .addAdditionalSearchToken('box')
   .setType('ACCESSOIRE')
 
   // Then we add the capabilities of the device
-  .addButton({ name: 'button-a', label: 'Button A' })
-  .addButton({ name: 'button-b', label: 'Button B' })
-  .addButtonGroup('Color Buttons')
+  .addButton({ name: 'button-on-off', label: 'Bouton ON OFF' })
+  .addButton({ name: 'button-right-arrow', label: 'Bouton FLECHE DROITE' })
+  .addButton({ name: 'button-down-arrow', label: 'Bouton FLECHE BAS' })
+  .addButton({ name: 'button-left-arrow', label: 'Bouton FLECHE GAUCHE' })
+  .addButton({ name: 'button-up-arrow', label: 'Bouton FLECHE HAUT' })
+  .addButton({ name: 'button-ok', label: 'Bouton OK' })
+  .addButton({ name: 'button-free', label: 'Bouton FREE' })
+  .addButton({ name: 'button-red', label: 'Bouton ROUGE' })
+  .addButton({ name: 'button-green', label: 'Bouton VERT' })
+  .addButton({ name: 'button-blue', label: 'Bouton BLEU' })
+  .addButton({ name: 'button-yellow', label: 'Bouton JAUNE' })
+  .addButton({ name: 'button-0', label: 'Bouton 0' })
+  .addButton({ name: 'button-1', label: 'Bouton 1' })
+  .addButton({ name: 'button-2', label: 'Bouton 2' })
+  .addButton({ name: 'button-3', label: 'Bouton 3' })
+  .addButton({ name: 'button-4', label: 'Bouton 4' })
+  .addButton({ name: 'button-5', label: 'Bouton 5' })
+  .addButton({ name: 'button-6', label: 'Bouton 6' })
+  .addButton({ name: 'button-7', label: 'Bouton 7' })
+  .addButton({ name: 'button-8', label: 'Bouton 8' })
+  .addButton({ name: 'button-9', label: 'Bouton 9' })
   .addButtonHandler(controller.onButtonPressed);
 
 function startSdkExample(brain) {
@@ -28,11 +46,11 @@ function startSdkExample(brain) {
   neeoapi.startServer({
     brain,
     port: 6336,
-    name: 'simple-accessory-one',
-    devices: [customLightDevice]
+    name: 'freebox-server',
+    devices: [freeboxDevice]
   })
   .then(() => {
-    console.log('# READY! use the NEEO app to search for "NEEO Accessory".');
+    console.log('# READY! use the NEEO app to search for "Freebox Revolution".');
   })
   .catch((error) => {
     //if there was any error, print message out to console
